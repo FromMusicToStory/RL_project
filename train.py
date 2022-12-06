3ce3905bf73d6ca1c11e69fae6b0aa141110936e
YERIN CHOI <lakahaga@u.sogang.ac.kr>
2022-12-07 00:08:32 +0900

    fix typo in config and train.py

--- train.py
+++ train.py
@@ -12,13 +12,13 @@ def main(config_path):
     model = DQNClassification(config, run_mode='train')
     model.train()
     lr_monitor = pl.callbacks.LearningRateMonitor()
-    checkponiter = pl.callbacks.ModelCheckpoint(dir_path=config['checkpoint_path'],
-                                                filename='{epoch:02d}-{train_loss:.2f}',
-                                                verbose=True, save_last=True, monitor='train_loss',
-                                                mode='max', prefix="model_")
+    checkponiter = pl.callbacks.ModelCheckpoint(dirpath=config['checkpoint_path'],
+                                                filename='model_{epoch:02d}-{train_loss:.2f}',
+                                                verbose=True, save_last=True, save_top_k=3, monitor='train_loss',
+                                                mode='min')
     # mode is max because train_loss will be the reward
     trainer = pl.Trainer(
-        accelerator='gpu', gpus=config['gpus'],
+        accelerator='gpu', gpus=config['ngpu'],
         check_val_every_n_epoch=config['check_val_every_n_epoch'],
         accumulate_grad_batches=config['accumulate_grad_batches'],
         log_every_n_steps=config['log_every_n_steps'],

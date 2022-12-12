ce05e453837ff321ab6076d05f8e4719303edd4a
YERIN CHOI <76737826+lakahaga@users.noreply.github.com>
2022-12-12 12:32:03 +0900

    config 불러오는 거 수정

--- infer.py
+++ infer.py
@@ -5,41 +5,52 @@ from tqdm import tqdm
 from sklearn.metrics import confusion_matrix, f1_score
 
 import torch
+import hydra
+
 from dataset import KLAID_dataset
 from model import DQNClassification
 from policy_model import PolicyGradientClassification
 from network import Classifier, DuelingClassifier, PolicyNet
 
-def get_network(net_name, checkpoint_path):
-    hparams = yaml.load(open('conf/dqn.yaml', 'r'), Loader=yaml.FullLoader)
-    hparams['gpu'] = ['cpu']
-    hparams['model_name'] = 'klue/roberta-base'
-    hparams['net'] = Classifier(model_name=hparams['model_name'], num_classes=10)
+def get_network(net_name, checkpoint_path, config):
+    # hparams = yaml.load(open('conf/dqn.yaml', 'r'), Loader=yaml.FullLoader)
+    # hparams['gpu'] = ['cpu']
+    # hparams['model_name'] = 'klue/roberta-base'
+    # hparams['net'] = Classifier(model_name=hparams['model_name'], num_classes=10)
 
     if 'DQN' in net_name:
-        net = DQNClassification(hparams=hparams, run_mode='test')
-        net.to('cpu')
-        net.load_state_dict(torch.load(checkpoint_path, map_location=torch.device("cpu")), strict=False)
+        model = DQNClassification(hparams=config.model, run_mode='test')
+        model.to('cpu')
+        model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device("cpu")), strict=False)
     else:
-        net = PolicyGradientClassification.load_from_checkpoint(checkpoint_path)
-        net.to('cpu')
-        net.load_state_dict(torch.load(checkpoint_path, map_location=torch.device("cpu")), strict=False)
+        model = PolicyGradientClassification.load_from_checkpoint(checkpoint_path)
+        model.to('cpu')
+        model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device("cpu")), strict=False)
 
-    return net
+    network = None
+    if 'DQN' == net_name:
+        network = model.classification_model
+    elif 'DQN_double' == net_name or 'DQN_dueling':
+        network = model.target_model
+    else:
+        network = model.p_net
+    print(network)
+    return network, model.dataset
 
 def pred_to_label(pred):
     pass
 
+@hydra.main(version_base=None, config_path='conf', config_name='infer_dqn')
 def main(args):
     # get RL network
-    net = get_network(args.net_name, args.checkpoint_path)
+    net, dataset = get_network(args.net_name, args.checkpoint_path, args)
 
     # infer
     trues, preds = [], []
     print("\nstart infer")
-    for i, data in tqdm(enumerate(net.dataset)):  # get dataset
+    for i, data in tqdm(enumerate(dataset)):  # get dataset
         input_ids, attention_mask, true_label = data['encoded_output'], data['encoded_attention_mask'], data['law_service_id']
-        prediction = net.model(input_ids=input_ids.unsqueeze(0), attention_mask=attention_mask.unsqueeze(0))
+        prediction = net(input_ids=input_ids.unsqueeze(0), attention_mask=attention_mask.unsqueeze(0))
         pred_label = prediction.argmax(1).item()
         trues.append(true_label)
         preds.append(pred_label)
@@ -54,10 +65,4 @@ def main(args):
 
 
 if __name__=='__main__':
-    parser = ArgumentParser()
-    parser.add_argument("--net_name", type=str, default="DQN", help="which RL algorithm, DQN, DoubleDQN, DuelingDQN, PolicyGradient")
-    parser.add_argument("--checkpoint_path", type=str, required=True)
-
-    args = parser.parse_args()
-
-    main(args)
+    main()
